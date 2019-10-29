4798d9e255b39d8a2222555ea64506d31c5f341d
Kung Lee <kung@hortonvillerobotics.com>
2019-10-29 18:45:21 -0500

    Edited values on the power scaling for synching wheel speeds. Pretty close.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpeedTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpeedTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
@@ -22,11 +24,10 @@ public class SpeedTest extends OpMode {
     public void loop() {
         s.initializeMachine();
         if(s.next_state_to_execute()){
-//            r.setDrivePower(0.2,0.2);
-            r.setPower(wheelSet1[0],0.8765382124    *.3);
-            r.setPower(wheelSet1[1],0.8571775537    *.3);
-            r.setPower(wheelSet2[0],0.9452373242    *.3);
-            r.setPower(wheelSet2[1],1               *.3);
+            r.setPower(wheelSet1[0],0.90071    *.3); //Top Left 0.8765382124
+            r.setPower(wheelSet1[1],0.90451    *.3); //Bottom Right 0.8571775537
+            r.setPower(wheelSet2[0],0.94713    *.3); //Top Right 0.9452373242
+            r.setPower(wheelSet2[1],1               *.3); //Bottom Left
         }
     }
 }
