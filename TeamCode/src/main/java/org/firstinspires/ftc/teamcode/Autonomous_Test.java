42dad0870e4963ef7dbfaf324841f7b040c15a55
hortonvilleroboticsgrace <41523342+hortonvilleroboticsgrace@users.noreply.github.com>
2019-11-07 21:55:04 -0600

    Upped the safeSpeed value to .7 from .5, commented out the Init_loop method for now, removed the center case and made a default, started writing mapped code and put comments in for what each statement was doing as well as where other code will go in the future for the collection and deposition of the SkyStones.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Test.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
@@ -11,7 +13,7 @@ public class Autonomous_Test extends OpMode {
     StateMachine sm = new StateMachine();
 
     String skyCase = "right";
-    double safeSpeed = .5;
+    double safeSpeed = .7;
 
     @Override
     public void init() {
@@ -19,33 +21,89 @@ public class Autonomous_Test extends OpMode {
         r.initialize(this);
     }
 
-    @Override
-    public void init_loop(){
-        StateMachine lazy = new StateMachine();
-    }
+//    @Override
+//    public void init_loop(){
+//        StateMachine lazy = new StateMachine();
+//    }
 
     @Override
     public void loop() {
         sm.initializeMachine();
 
+        /*
+        Insert Image Processing Code In This Section
+         */
+
+         /*
+            Objective:  Use Image Processing Code to determine SkyStone Placement
+                        and set skyCase to the correct orientation.
+
+                        Travel to correct SkyStone, grab it and take it across the Bridge
+
+                        Place near/on the foundation
+
+                        **Travel back, grab the second SkyStone, and take across Bridge IT TIME
+
+                        **Move Foundation to corner IF TIME
+
+                        Place and park under Bridge
+        */
+
         switch(skyCase){
 
+
+
             case "left":
-                sm.translate(154,safeSpeed,22);
-                sm.translate(-90,safeSpeed,83);
+                try {
+                    sm.translate(154, safeSpeed, 22); //Travel to and Grab SkyStone
 
-                break;
+//                    sm.wait(500);
+                    sm.translate(-90, safeSpeed, 83); //Travel to Foundation and Place SkyStone
 
-            case "center":
-                sm.translate(174.5,safeSpeed,20);
-                sm.translate(-90,safeSpeed,75);
+//                    sm.wait(500);
+                    sm.translate(90, safeSpeed, 102); //Travel back for second stone
+
+//                    sm.wait(500); //Grab second SkyStone
+
+                    sm.translate(-90, safeSpeed, 102); //Travel to Foundation & Place Second SkyStone
+//                    sm.wait(500);
+
+                    sm.translate(90, safeSpeed, 40); //Park Under Bridge
+                } catch (Exception e) {
+                    Log.e("LEFT SKYCASE", "Failure");
+            }
+
                 break;
 
             case "right":
-                sm.translate(-159,safeSpeed,21);
-                sm.translate(-90,safeSpeed,67);
+                try {
+                    sm.translate(-159, safeSpeed, 21); //Travel to and Grab SkyStone
+
+//                    sm.wait(500);
+
+                    sm.translate(-90, safeSpeed, 67); //Travel to Foundation and Place SkyStone
+
+//                    sm.wait(500);
+
+                    sm.translate(90, safeSpeed, 90); //Travel back for second stone
+
+//                    sm.wait(500); //Grab second SkyStone
+
+                    sm.translate(-90, safeSpeed, 90); //Travel to Foundation & Place Second SkyStone
+//                    sm.wait(500);
+
+                    sm.translate(90, safeSpeed, 40); //Park Under Bridge
+
+
+                } catch (Exception e) {
+                    Log.e("RIGHT SKYCASE", "Failure");
+                }
                 break;
 
+            default:
+                sm.translate(174.5,safeSpeed,20);
+                sm.translate(-90,safeSpeed,75);
+                break;
         }
 
         telemetry.addData("mtrLeftFront", r.getEncoderCounts("mtrFrontLeft"));
