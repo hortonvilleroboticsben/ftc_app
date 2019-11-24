8730dd83fa58f3f408753a2098c71a0770e8d049
hortonvilleroboticsgrace <41523342+hortonvilleroboticsgrace@users.noreply.github.com>
2019-11-24 16:02:20 -0600

    Added a second motor to test the collection with the wheels.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTest.java
@@ -8,14 +8,16 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp (name = "Lift Test", group = "Testing")
 public class LiftTest extends OpMode {
 
-    DcMotor lift;
+    DcMotor lift, otherMotor;
 
 
     @Override
     public void init() {
 
         lift = hardwareMap.dcMotor.get("mtrLift");
+        otherMotor = hardwareMap.dcMotor.get("mtrOther");
         lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        otherMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
     }
 
@@ -27,7 +29,9 @@ public class LiftTest extends OpMode {
                         Gamepad1.a  --->    UP
                         Else                No Motor Power
          */
-        lift.setPower(gamepad1.y ? -0.6 : gamepad1.a && !gamepad1.start ? 0.5 : -0.0);
+        lift.setPower(gamepad1.y ? -0.6 : gamepad1.a && !gamepad1.start ? 0.5 : 0.0);
+        otherMotor.setPower(gamepad1.y ? 0.5 : gamepad1.a && !gamepad1.start ? -0.6 : 0.0);
+
 
     }
 }
