e0a8d4bfda93cb9b2e6e793edd1adfa7128dd051
hortonvilleroboticsben <ben@hortonvillerobotics.com>
2020-01-26 14:06:56 -0600

    Fixed and fully implemented Camera View Drawing.  All edits to drawing should be made to the org.firstinspires.ftc.robotcontroller.internal.Doodler.java class.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Doodler.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Doodler.java
@@ -4,20 +4,36 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 public class Doodler extends View {
 
+
+    Paint p;
+    int x,y,w,h;
+    int xp,yp,wp,hp;
+    Rect r;
     public Doodler(Context c){
         super(c);
+        p = new Paint();
+        p.setStrokeWidth(5);
+        p.setColor(Color.RED);
+        p.setStyle(Paint.Style.STROKE);
+        x = 50;
+        y = 300;
+        w = 190;
+        h = 50;
+        xp = y;
+        yp = this.getHeight()-x;
+        wp = h;
+        hp = w;
+        r = new Rect(xp,yp,xp+wp,yp+hp);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint p = new Paint();
-        p.setStrokeWidth(5);
-        p.setColor(Color.RED);
-        canvas.drawRect(20,20,20,20,p);
+        canvas.drawRect(r,p);
         super.onDraw(canvas);
     }
 }
