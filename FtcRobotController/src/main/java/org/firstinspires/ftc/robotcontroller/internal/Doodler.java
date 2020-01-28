ffa47f6fdbc08eef7047ab3dc24fd7f302f940f0
hortonvilleroboticsben <ben@hortonvillerobotics.com>
2020-01-27 18:28:19 -0600

    Figured out problem with Doodler.java. Turns out Views have a height of 0 when constructed before loading.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Doodler.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Doodler.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.view.View;
 
 public class Doodler extends View {
@@ -12,28 +13,27 @@ public class Doodler extends View {
 
     Paint p;
     int x,y,w,h;
-    int xp,yp,wp,hp;
-    Rect r;
+    float xp,yp,wp,hp;
     public Doodler(Context c){
         super(c);
         p = new Paint();
         p.setStrokeWidth(5);
         p.setColor(Color.RED);
         p.setStyle(Paint.Style.STROKE);
+        p.setTextSize(16);
         x = 50;
         y = 300;
         w = 190;
         h = 50;
-        xp = this.getWidth()-y-h;
-        yp = x;
-        wp = h;
-        hp = w;
-        r = new Rect(xp,yp,xp+wp,yp+hp);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawRect(r,p);
+        xp = y;
+        yp = getHeight()-w-x;
+        wp = h;
+        hp = w;
+        canvas.drawRect(xp,yp,wp+xp,hp+yp,p);
         super.onDraw(canvas);
     }
 }
